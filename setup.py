6e8c857b20cf4b5e5aa0a3dd45b87911aa0fa023
JuanBetancourt <juan.k.ita@hotmail.es>
2025-02-27 23:55:55 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="practiceRepo3",                               # Nombre del paquete
+    version="0.1.0",                                    # Versión inicial
+    packages=find_packages(),                           # Paquetes a incluir
+    description="Un paquete pip simple de saludo",      # Breve descripción
+    author="vanDevBett",                                # Tu nombre
+    author_email="juan.k.ita@hotmail.es",               # Tu correo electrónico
+    url="https://github.com/vanDevBett/practiceRepo3",  # URL del proyecto
+)
